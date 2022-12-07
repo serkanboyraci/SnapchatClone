2e1afe89d54df2dbcbdfe016b3c9ff6b620f7871
serkanboyraci <serkanboyraci@gmail.com>
2022-12-07 23:29:24 +0300

    to exit app with settingsVC by logout button

--- SnapchatClone/ViewControllers/SettingsVC.swift
+++ SnapchatClone/ViewControllers/SettingsVC.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
 
 class SettingsVC: UIViewController {
 
@@ -16,16 +19,16 @@ class SettingsVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func logoutClicked(_ sender: Any) {
+        
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toSignInVC", sender: nil)
+            
+            
+        } catch {
+            
+        }
     }
     
 }
