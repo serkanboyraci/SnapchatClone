cbef604d65ec066162268a341fdb1eae951774cb
serkanboyraci <serkanboyraci@gmail.com>
2022-12-09 23:51:57 +0300

    Unitle creating Singleton

--- SnapchatClone/ViewControllers/FeedVC.swift
+++ SnapchatClone/ViewControllers/FeedVC.swift
@@ -9,25 +9,48 @@ import UIKit
 import FirebaseAuth
 import FirebaseStorage
 import FirebaseFirestore
+import FirebaseCore
+
+
 
 class FeedVC: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+    
+    let fireStoreDatabase = Firestore.firestore()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getUserInfo()
+    }
+    func getUserInfo() {
+        
+        fireStoreDatabase.collection("UserInfo").whereField("email", isEqualTo: Auth.auth().currentUser!.email!).getDocuments { (snapshot, error) in
+            if error != nil {
+                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+            } else {
+                if snapshot?.isEmpty == false && snapshot != nil {
+                    for document in snapshot!.documents {
+                        if let username = document.get("username") as? String {
+                            
+                        }
+                            
+                    }
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
-    */
-
+    
+    
 }
